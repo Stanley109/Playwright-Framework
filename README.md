9e4da008f349d95e7d09eeb4d0a237f5f18ace7d
Stanley109 <jasonstanleykho@gmail.com>
2026-07-10 00:49:08 +0800

    Update README and configuration for Playwright setup

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-## Steps
-1.npm init playwright@latest
-2.
+## Notes
+1. `npm init playwright@latest` to initialize your repository
+2. `npx playwright test --project=chromium --headed` to only run the test with chromium and in headed mode
+3. Alternatively, you can run `npm run test:chrome headless` or `npm run test:chrome headed`
+
--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
-    headless: true, // or false
+    headless: true, //  by default, Playwright runs tests in headless mode even when this is not explicitly set.
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
