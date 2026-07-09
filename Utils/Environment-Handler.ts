6315dc1273fa45324459f5ee48b6aa67884c04b2
Stanley109 <jasonstanleykho@gmail.com>
2026-07-10 02:08:05 +0800

    Add environment configuration and initial test setup

---
+++ Utils/Environment-Handler.ts
@@ -0,0 +1,6 @@
+
+
+const testEnv = process.env.TEST_ENV || 'sit';
+const BASE_URL = {
+
+};
--- playwright.config.ts
+++ playwright.config.ts
@@ -7,31 +7,22 @@ import { defineConfig, devices } from '@playwright/test';
 // import dotenv from 'dotenv';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
+const baseURL = process.env.BASE_URL || 'https://spanish-cards.netlify.app';
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    headless: true, //  by default, Playwright runs tests in headless mode even when this is not explicitly set.
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    baseURL: baseURL,           //sets the base URL for all tests.
+    headless: true,             //by default, Playwright runs tests in headless mode even when this is not explicitly set.
     trace: 'on-first-retry',
+    launchOptions: {
+      args: ['--start-maximized', '--disable-gpu', '--window-size=1920,1080']
+    },
   },
 
   /* Configure projects for major browsers */
