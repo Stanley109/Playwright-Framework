4ce85881eb7791ed2614073cd9952ebbc493c898
Stanley109 <jasonstanleykho@gmail.com>
2026-08-19 18:06:45 +0800

    fix bracket issue

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   reporter: [
     ['line'], // Terminal progress logger
     ['monocart-reporter', { name: 'Playwright Test Report', outputFile: 'monocart-report/index.html'}],   //Monocart report. also sets the name of the report
-    ['html', { outputFolder: 'playwright-report', open: 'never' },    // Playwright's built-in HTML report
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],    // Playwright's built-in HTML report
     ['json', { outputFile: 'playwright-report/playwright-report.json'}]
   ],
   
